c2c0cd8836c57c336833b53c70ed758b07ef68e0
Douwe den Blanken <douwedb@gmail.com>
2021-03-09 20:28:55 +0100

    Moved #endif to end of Vector.hpp file

--- include/Constellation/Vector.hpp
+++ include/Constellation/Vector.hpp
@@ -5,8 +5,6 @@
 #ifndef CONSTELLATION_VECTOR_HPP
 #define CONSTELLATION_VECTOR_HPP
 
-#endif //CONSTELLATION_VECTOR_HPP
-
 namespace Constellation {
     template<typename U>
     class Vector : public Matrix<U> {
@@ -14,3 +12,5 @@ namespace Constellation {
         Vector(int d, U *v) : Matrix<U>(1, d, v) {}
     };
 }
+
+#endif //CONSTELLATION_VECTOR_HPP
