b9d1680ce80548b05fc9387f7f3eb1aecb3d4f28
Douwe den Blanken <douwedb@gmail.com>
2021-10-03 12:26:19 +0200

    Add Constants header

---
+++ include/Constellation/Constants/Constants.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Douwe den Blanken on 08/03/2021.
+//
+
+#ifndef CONSTELLATION_CONSTANTS_HPP
+#define CONSTELLATION_CONSTANTS_HPP
+
+namespace Constellation {
+    float foot;
+    float inch;
+    float yard;
+
+    double foot;
+}
+
+#endif //CONSTELLATION_CONSTANTS_HPP
